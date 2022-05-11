39ebe305a329ef58a2575f959b8796d0d1437529
Pierce Harmon <pierceh.dev@gmail.com>
2022-05-11 09:00:56 -0500

    W.I.P.

--- server/index.ts
+++ server/index.ts
@@ -1,29 +1,14 @@
-import './bootstrap-globals';
-import { createExpressHandler } from './createExpressHandler';
-import express, { RequestHandler } from 'express';
+import express from 'express';
 import path from 'path';
-import { ServerlessFunction } from './types';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8081;
 
 const app = express();
+
 app.use(express.json());
 
-// This server reuses the serverless endpoints from the "plugin-rtc" Twilio CLI Plugin, which is used when the "npm run deploy:twilio-cli" command is run.
-// The documentation for this endpoint can be found in the README file here: https://github.com/twilio-labs/plugin-rtc
-const tokenFunction: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/token').handler;
-const tokenEndpoint = createExpressHandler(tokenFunction);
-
-const recordingRulesFunction: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/recordingrules')
-  .handler;
-const recordingRulesEndpoint = createExpressHandler(recordingRulesFunction);
-
-const noopMiddleware: RequestHandler = (_, __, next) => next();
-const authMiddleware =
-  process.env.REACT_APP_SET_AUTH === 'firebase' ? require('./firebaseAuthMiddleware') : noopMiddleware;
-
-app.all('/token', authMiddleware, tokenEndpoint);
-app.all('/recordingrules', authMiddleware, recordingRulesEndpoint);
+// app.all('/token', authMiddleware, tokenEndpoint);
+// app.all('/recordingrules', authMiddleware, recordingRulesEndpoint);
 
 app.use((req, res, next) => {
   // Here we add Cache-Control headers in accordance with the create-react-app best practices.
